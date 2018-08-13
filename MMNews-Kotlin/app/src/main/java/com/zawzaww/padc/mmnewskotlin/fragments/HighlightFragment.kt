daa3a9e5eabf0f49b5e69a5bd9224465bc7b2494
Zaw Zaw <mczawzaw12@outlook.com>
2018-08-13 16:26:44 +0630

    MMNews-Kotlin: fragments: Implement static fagement for Highlight news headline

--- MMNews-Kotlin/app/src/main/java/com/zawzaww/padc/mmnewskotlin/fragments/HighlightFragment.kt
+++ MMNews-Kotlin/app/src/main/java/com/zawzaww/padc/mmnewskotlin/fragments/HighlightFragment.kt
@@ -1,21 +1,17 @@
 package com.zawzaww.padc.mmnewskotlin.fragments
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.zawzaww.padc.mmnewskotlin.R
-import com.zawzaww.padc.mmnewskotlin.adapters.HighlightNewsAdapter
 
 class HighlightFragment : BaseFragment() {
 
-    private lateinit var mAdapter: HighlightNewsAdapter
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_highlight, container, false)
-
+        val view = inflater.inflate(R.layout.fragment_highlight, container, false)
+        return view
     }
+
 }
